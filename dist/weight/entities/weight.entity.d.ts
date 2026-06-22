d2abb5b8184b9dd19c1b9ee0af4f954330d311c0
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2026-06-22 10:27:55 -0300

    remove files

--- dist/weight/entities/weight.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Weight {
-    id: number;
-    value: number;
-    date: string;
-    time: string | null;
-    createdAt: Date;
-    updatedAt: Date;
-    creatorId: number | null;
-}
