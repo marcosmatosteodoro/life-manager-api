1b7050bc5d48100493b1c913951037d639904af0
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2026-06-22 10:21:46 -0300

    Refactor code structure for improved readability and maintainability

--- dist/main.js
+++ dist/main.js
@@ -18,7 +18,7 @@ async function bootstrap() {
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, swaggerConfig);
     swagger_1.SwaggerModule.setup('docs', app, document);
-    await app.listen(process.env.PORT ?? 3000);
+    await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
